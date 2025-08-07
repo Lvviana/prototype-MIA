a2e2650d7ed0f91bd9d9d1c64a0c7ca2c6f1e0fd
Lvviana <139356756+Lvviana@users.noreply.github.com>
2025-08-07 11:07:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const div = document.getElementById("conteudo");
+    div.innerHTML = "<p>Mi.a está carregando seu universo simbiótico...</p>";
+});
