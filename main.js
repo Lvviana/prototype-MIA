6d24bf718eca1411e5ef818a8dbe93faf45cd3b0
Lvviana <139356756+Lvviana@users.noreply.github.com>
2025-08-07 11:53:33 -0300

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const div = document.getElementById("conteudo");
-    div.innerHTML = "<p>Mi.a está carregando seu universo simbiótico...</p>";
-});
